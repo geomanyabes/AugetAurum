286cb66b1d234eb92c8cc20577770fbfc3a626a0
Geoman Yabes <gyabes@sedulen.com>
2022-01-20 17:19:16 +0800

    Add project files.

---
+++ AugetAurum.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AugetAurum.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
